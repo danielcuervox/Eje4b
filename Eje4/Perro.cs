47981bd2c90e72471f37da5c6840f3fbf9e0f8b3
danielcuervox <danielcuervox@hotmail.com>
2024-09-29 22:22:20 +0200

    clase perro

---
+++ Eje4/Perro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eje4
+{
+    internal class Perro : Animal
+    {
+        public override void caminar()
+        {
+            Console.WriteLine("El perro camina");
+        }
+
+        public override void comer()
+        {
+            Console.WriteLine("El perro come");
+        }
+
+        public override void dormir()
+        {
+            Console.WriteLine("El perro duerme");
+        }
+
+        public override void hacerRuido()
+        {
+            Console.WriteLine("El perro hace ruido");
+        }
+    }
+}
